60652d5d19e9b6536cbb4563e700a192e370483e
chenzhisheng <chenzhisheng@galasports.com>
2023-03-30 13:55:49 +0800

    Feign远程调用 1、引入Feign客户端依赖 spring-cloud-starter-openfeign 2、启动类添加@EnableFeignClients注解 3、编写要调用的其他服务的FeignClient接口，遵循SpringMVC规范 4、使用FeignClient接口定义的方法代替RestTemplate

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -18,18 +19,33 @@ public class OrderService {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private UserClient userClient;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 2.利用RestTemplate发起HTTP请求，远程调用user-service服务（user-service暴露restful接口）
-        // 硬编码服务地址，每次变更都要在这里修改，而且如果服务是多实例，就不能这样硬编码成某一个实例的地址
-//        String url = "http://localhost:8081/user/" + order.getUserId();
-        // 用 服务名称 代替 IP和端口，服务地址更改也无需修改，并且适用于多实例服务，根据服务名称拉取服务列表之后由Ribbon进行负载均衡（RestTemplate加上@LoadBalanced）
-        String url = "http://userservice:8081/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        // 2.通过Feign远程调用userservice服务
+        User user = userClient.findById(order.getUserId());
         // 3.封装
         order.setUser(user);
         // 4.返回
         return order;
     }
+
+    // 通过RestTemplate手动远程调用userservice服务
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 2.利用RestTemplate发起HTTP请求，远程调用user-service服务（user-service暴露restful接口）
+//        // 硬编码服务地址，每次变更都要在这里修改，而且如果服务是多实例，就不能这样硬编码成某一个实例的地址
+////        String url = "http://localhost:8081/user/" + order.getUserId();
+//        // 用 服务名称 代替 IP和端口，服务地址更改也无需修改，并且适用于多实例服务，根据服务名称拉取服务列表之后由Ribbon进行负载均衡（RestTemplate加上@LoadBalanced）
+//        String url = "http://userservice:8081/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        // 3.封装
+//        order.setUser(user);
+//        // 4.返回
+//        return order;
+//    }
 }
