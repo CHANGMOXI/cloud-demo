4b1dd28fe0c58fc8d61c854422fe4ac8237e6e10
chenzhisheng <2143204964@qq.com>
2023-04-01 21:13:18 +0800

    Feign最佳实践：抽取（将FeignClient、POJO、Feign的默认配置都定义到一个项目中，供所有消费者使用） 1、创建一个module，命名为feign-api，引入feign的starter依赖 2、将服务(order-service)中编写的其他服务(user-service)的UserClient、User、针对其他服务的FeignClientConfig都复制到feign-api项目中 3、在服务(order-service)中引入feign-api的依赖 4、修改服务(order-service)中所有与其他服务有关的import部分，改成import feign-api里的，同时启动类的@EnableFeignClients注解可以指定FeignClient，避免启动类在默认扫描包方式下，扫描不到不同包的FeignClient的问题

--- feign-api/src/main/java/cn/itcast/feign/clients/UserClient.java
+++ feign-api/src/main/java/cn/itcast/feign/clients/UserClient.java
@@ -1,13 +1,12 @@
-package cn.itcast.order.clients;
+package cn.itcast.feign.clients;
 
-import cn.itcast.order.config.FeignClientConfig;
-import cn.itcast.order.pojo.User;
+import cn.itcast.feign.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("userservice")
-//@FeignClient(value = "userservice", configuration = FeignClientConfig.class)
+//@FeignClient(value = "userservice", configuration = FeignClient.class)
 public interface UserClient {
     @GetMapping("/user/{id}")
     User findById(@PathVariable("id") Long id);
--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,6 +1,7 @@
 package cn.itcast.order;
 
-import cn.itcast.order.config.FeignClientConfig;
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.config.FeignClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
-@EnableFeignClients(defaultConfiguration = FeignClientConfig.class)
+// Feign最佳实践：抽取
+// 定义的FeignClient不在启动类扫描包范围时，指定FeignClient的两种方式:
+// ①通过basePackages属性指定FeignClient所在包("cn.itcast.feign.clients")，包下所有FeignClient都会扫描到
+// ②通过clients属性指定具体的FeignClient，单个或多个(写成{xxx.class, yyy.class})
+@EnableFeignClients(clients = UserClient.class, defaultConfiguration = FeignClientConfig.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
